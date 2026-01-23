d53e5b1719ac29c06d0fff492a42d86626443624
ianwelles <ianwelles@gmail.com>
2026-01-23 21:02:26 +0000

    Explicitly register service worker in index.tsx

--- index.tsx
+++ index.tsx
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; 
 import 'leaflet/dist/leaflet.css';
+import { registerSW } from 'virtual:pwa-register';
+
+// Register the Service Worker
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm('New content available. Reload?')) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('App is ready for offline work.');
+  },
+});
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
